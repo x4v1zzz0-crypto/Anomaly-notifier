95e9638fb1c776dc02e8f138b1645939ebf8fe3e
x4v1zzz0-crypto <x4v1zzz0@gmail.com>
2026-03-15 04:23:33 -0300

    Update join.js

--- api/join.js
+++ api/join.js
@@ -1,23 +1,17 @@
-// api/join.js
-export default function handler(req, res) {
-  const { placeId, jobId } = req.query;
+export default async function handler(req,res){
 
-  if (!placeId || !jobId) {
-    return res.status(400).json({ error: "Missing params: placeId & jobId" });
-  }
+const { placeId, jobId } = req.query
 
-  const joinUrl = `https://www.roblox.com/games/${placeId}?gameInstanceId=${jobId}`;
+if(!placeId || !jobId){
+return res.status(400).send("Missing parameters")
+}
 
-  // Opcional: reenviar a Discord como mensaje simple
-  const webhook = "https://discord.com/api/webhooks/1455084311698214934/KvZgFXn0S-jdon25qGwbayS3JXwFexW70nKpgvSe6jCUCIMcpIBuy30CI5C2oLpxZf90";
+const link = `roblox://placeId=${placeId}&gameInstanceId=${jobId}`
 
-  fetch(webhook, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      content: `Nuevo server detectado! Join: ${joinUrl}`
-    })
-  }).catch(console.error);
+res.writeHead(302,{
+Location:link
+})
 
-  res.status(200).json({ success: true, join: joinUrl });
+res.end()
+
 }
